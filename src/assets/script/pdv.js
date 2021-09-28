8e303186683f355bc79e1339ff17373818e047b1
Gabriel Miranda <gabriel.miranda041200@gmail.com>
2021-09-27 23:18:32 -0300

    Finalizado desconto de valor

--- src/assets/script/pdv.js
+++ src/assets/script/pdv.js
@@ -3,17 +3,18 @@ let spaceComanda = document.getElementById('spaceComanda')
 let cupom = document.getElementById('cupom')
 let mensagem = document.getElementById('mensagem')
 let valorCompleto = document.getElementById('valorCompleto')
+let desconto = document.getElementById('desconto')
 const v = []
 let total = 0
 const puxarComanda = (codigo) => {
     fetch(`http://127.0.0.1:5000/pushComanda/${codigo}`)
         .then(res => res.json())
         .then(data => {
-            pushComanda(data,codigo)
+            pushComanda(data, codigo)
         })
 
 }
-const pushComanda = (data,comanda) => {
+const pushComanda = (data, comanda) => {
     spaceComanda.innerHTML = comanda
     mensagem.value = "Venda em Andamento"
     for (let i = 0; i < data.length; i++) {
@@ -27,21 +28,21 @@ const pushComanda = (data,comanda) => {
                         <div>${data[i].qtde}</div>
                         <div>R$ ${value.valor}</div>
                     </div>`
-                valorTotal(value.valor,data[i].qtde)
+                valorTotal(value.valor, data[i].qtde)
             })
-        }
+    }
 }
-const valorTotal = (valor,qtd) =>{
-    let requi = valor*qtd
-    total = total+requi
+const valorTotal = (valor, qtd) => {
+    let requi = valor * qtd
+    total = total + requi
     valorCompleto.value = `R$${total.toFixed(2)}`
 }
-document.addEventListener('keypress',e=>{
-    if(e.charCode == 100){
+document.addEventListener('keypress', e => {
+    if (e.charCode == 100) {
         spaceComanda.innerHTML = 'CAIXA LIVRE'
         mensagem.value = "CAIXA LIVRE"
         total = 0
-        cupom.innerHTML=`<div id="cupom" class="cab">** Cupom Fiscal **</div>
+        cupom.innerHTML = `<div id="cupom" class="cab">** Cupom Fiscal **</div>
         --------------------------------------------------------------------------
         <div class="list cab">
             <div>CÓDIGO</div>
@@ -55,7 +56,16 @@ document.addEventListener('keypress',e=>{
 codigoComanda.addEventListener('keypress', e => {
     if (e.charCode == 13) {
         puxarComanda(codigoComanda.value)
-        codigoComanda.value=''
+        codigoComanda.value = ''
     }
 })
-
+desconto.addEventListener('keypress', e => {
+    if (e.charCode == 13) {
+        totalDesc = total
+        if (total <= totalDesc && valorCompleto.value>0) {
+            let valorDescontado = total - desconto.value
+            valorCompleto.value = `R$${valorDescontado.toFixed(2)}`
+            desconto.value = ''
+        }
+    }
+})
