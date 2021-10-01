424de7ec926457782961369be8298bf9e7019ebb
Gabriel Miranda <gabriel.miranda041200@gmail.com>
2021-09-30 23:23:03 -0300

    Finalizado aréa de Cadastro e pesquisa de Usuarios

--- src/assets/script/usuarios.js
+++ src/assets/script/usuarios.js
@@ -5,8 +5,48 @@ const inserir = async () => {
     const senha = document.getElementById('senha').value
     const nome = document.getElementById('nome').value
 
-    //await inserirProdutos(codigo, { "nome": nome, "valor": valor })
+    await inserirUsers(usuario, { "login": usuario, "senha": senha, "nome_funcionario": nome, "id": null })
 }
+const inserirUsers = (codigo, body) => {
+    try {
+        fetch(`http://127.0.0.1:5000/usuario/${codigo}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+const puxar = () => {
+    fetch('http://127.0.0.1:5000/usuarios')
+        .then(res => res.json())
+        .then(data => {
+            try {
+                lista.innerHTML = `
+                <div class="list_cab">
+                <div>ID</div>
+                <div>Usuario</div>
+                <div>Nome</div>
+                </div>`
+                const users = data.usuario
+                for (let i = 0; i < users.length; i++) {
+                    lista.innerHTML +=
+                        `<div class="list_us">
+                    <div>${users[i].id}</div>
+                    <div>${users[i].login}</div>
+                    <div>${users[i].nome_funcionario}</div>
+                    </div>`
+                }
 
-
-
+            }
+            catch (e) {
+                console.log(e)
+            }
+        })
+}
+const sair = () => {
+    window.location.href = "menu.html";
+}
