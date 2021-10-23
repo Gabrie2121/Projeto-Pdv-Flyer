e6248e0b30e32b7d8da83a59552d3c15eadf3b28
Gabriel Miranda <gabriel.miranda041200@gmail.com>
2021-10-23 12:51:01 -0300

    Delete de produtos finalizado

--- src/assets/script/produtos.js
+++ src/assets/script/produtos.js
@@ -3,8 +3,6 @@ const inserir = async () => {
     const codigo = document.getElementById('codigo').value
     const nome = document.getElementById('nome').value
     const valor = document.getElementById('valor').value
-    console.log(valor)
-
     await inserirProdutos(codigo, { "nome": nome, "valor": valor })
 }
 const inserirProdutos = (codigo, body) => {
@@ -16,9 +14,13 @@ const inserirProdutos = (codigo, body) => {
             },
             body: JSON.stringify(body)
         })
-    }catch(e){
+    } catch (e) {
         console.log(e)
     }
+    puxarTodos()
+}
+const puxarTodos = () =>{
+    puxarProdutos("produtos")
 }
 const puxar = async () => {
     const codigo = document.getElementById('codigo').value
@@ -34,6 +36,7 @@ const puxarProdutos = (codigo) => {
                 <div>CÓDIGO</div>
                 <div>DESCRIÇÃO</div>
                 <div>VALOR</div>
+                <div></div>
             </div>`
                 prods = data.produtos
                 console.log(prods)
@@ -43,12 +46,14 @@ const puxarProdutos = (codigo) => {
                     <div>${prods[i].codigo}</div>
                     <div>${prods[i].nome.toUpperCase()}</div>
                     <div>R$ ${prods[i].valor.toFixed(2)}</div>
+                    <div><button class="apagar" onclick="excluir(${prods[i].codigo})">Apagar</button></div>
                 </div>`
                     } catch (e) {
                         return `<div class="list_prods">
                         <div>${prods[i].codigo}</div>
                         <div>${prods[i].nome}</div>
                         <div>R$ ${prods[i].valor}</div>
+                        <div><button class="apagar" onclick="excluir(${prods[i].codigo})">Apagar</button></div>
                     </div>`
                     }
                 }
@@ -58,14 +63,24 @@ const puxarProdutos = (codigo) => {
                 <div>CÓDIGO</div>
                 <div>DESCRIÇÃO</div>
                 <div>VALOR</div>
+                <div></div>
             </div><div class="list_prods">
                     <div>${data.codigo}</div>
                     <div>${data.nome.toUpperCase()}</div>
                     <div>R$ ${data.valor.toFixed(2)}</div>
+                    <div><button class="apagar" onclick="excluir()">Apagar</button></div>
                 </div>`
             }
         })
 }
 const sair = () => {
     window.location.href = "menu.html";
+}
+const excluir = (id) => {
+    console.log(id)
+    fetch(`http://31.220.21.132:5000/produto/${id}`, {
+        method: 'DELETE',
+    })
+        .then(res => res.text()) // or res.json()
+        .then(res => puxarTodos())
 }
